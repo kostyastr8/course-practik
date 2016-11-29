1755546d69e1dc74512804da881179d0c56ca85d
Konstantyn <kostyastr8@gmail.com>
2016-11-30 00:11:44 +0200

    add slider to about us block

---
+++ js/slider.js
@@ -0,0 +1,27 @@
+
+$(document).ready(function(){
+    var $slider = $('.slider');
+
+$slider.slick({
+    infinite: true,
+    slidesToScroll: 1,
+    slidesToShow: 4,
+    arrows:false,
+    responsive:[
+        {
+            breakpoint:1200,
+            settings:{
+                slidesToShow:3,
+                dots:true
+            }
+        },
+        {
+            breakpoint:960,
+            settings:{
+                slidesToShow:2,
+                dots:true
+            }
+        }
+    ]
+});
+});
